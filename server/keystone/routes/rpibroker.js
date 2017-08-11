635fe18f47a40c502dc9030c3b080f3bf1de015a
Chris Troutner <chris.troutner@gmail.com>
2017-08-11 16:47:05 -0700

    Converting logwork dashboard from rpiovn to marketplace

--- server/keystone/routes/rpibroker.js
+++ server/keystone/routes/rpibroker.js
@@ -22,4 +22,6 @@ module.exports = function(app) {
   // NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
   //app.get('/loggedinuser', middleware.requireUser, routes.views.loggedinuser);
+  app.get('/market', middleware.requireUser, routes.views.market);
 }
+
