74fef567cf7b3a9d04a4a781d101bf43411f4366
Chris Troutner <chris.troutner@gmail.com>
2017-08-17 20:59:59 -0700

    Trying to make reverse ssh connection to Docker container on RPi

--- client/reverse-tunnel.js
+++ client/reverse-tunnel.js
@@ -9,7 +9,7 @@ tunnel({
   dstHost: '0.0.0.0', // bind to all IPv4 interfaces
   dstPort: 6101, //The new port that will be opened
   //srcHost: '127.0.0.1', // default
-  srcPort: 22 // The port on the Pi to tunnel to.
+  srcPort: 3100 // The port on the Pi to tunnel to.
 }, function(error, clientConnection) {
   if(error)
     console.error('Error! ', error);
