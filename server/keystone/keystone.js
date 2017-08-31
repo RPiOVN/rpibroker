691e32fa7bf84fe6af6071eb7073166df3151326
Chris Troutner <chris.troutner@gmail.com>
2017-08-30 21:52:53 -0700

    Customizing for live server

--- server/keystone/keystone.js
+++ server/keystone/keystone.js
@@ -38,7 +38,7 @@ keystone.init({
   'auth': true,
   'user model': 'User',
   'file limit': '50MB',
-  'mongo': 'mongodb://172.17.0.1:3500/rpibroker',
+  'mongo': 'mongodb://172.17.0.1:3502/rpibroker',
   'signin redirect': '/market',
   'signout redirect': '/'
 });
