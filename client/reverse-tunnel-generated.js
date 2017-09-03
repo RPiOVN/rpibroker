e4519de3092e03bd541052570efd92ff89fa5878
Chris Troutner <chris.troutner@gmail.com>
2017-09-03 20:10:56 +0000

    chmod script

--- client/reverse-tunnel-generated.js
+++ client/reverse-tunnel-generated.js
@@ -2,10 +2,10 @@ var tunnel = require('reverse-tunnel-ssh');
 tunnel({
   host: 'undefined',
   port: 6100,
-  username: 'tlBCyB4W1E',
-  password: '6xBSEhVkxa',
+  username: '5svGuX2Bik',
+  password: '4vo34CMXwU',
   dstHost: '0.0.0.0',
-  dstPort: 3016, 
+  dstPort: 3017, 
   srcPort: 3100 
 }, function(error, clientConnection) {
   if(error)
